40f55cb046b591061558a1b9d0b362603d9ad1a1
fightjc <cyl1234567890@163.com>
2021-11-01 16:34:51 +0800

    change genshin search result to image

---
+++ src/main/java/org/fightjc/xybot/util/genshin/GenshinSearchDrawHelper.java
@@ -0,0 +1,26 @@
+package org.fightjc.xybot.util.genshin;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.image.BufferedImage;
+
+public class GenshinSearchDrawHelper {
+
+    private static final Logger logger = LoggerFactory.getLogger(GenshinSearchDrawHelper.class);
+
+    public static BufferedImage drawCharacterInfo() {
+        // TODO:
+        return new BufferedImage(0, 0, BufferedImage.TYPE_INT_ARGB);
+    }
+
+    public static BufferedImage drawWeaponInfo() {
+        // TODO:
+        return new BufferedImage(0, 0, BufferedImage.TYPE_INT_ARGB);
+    }
+
+    public static BufferedImage drawMaterialInfo() {
+        // TODO:
+        return new BufferedImage(0, 0, BufferedImage.TYPE_INT_ARGB);
+    }
+}
